9bd2147fc5a6cee8a0d7fe4fe6868b69597be359
Unknown <D226671@edu.rocwb.nl>
2018-03-12 14:40:22 +0100

    Add basic html structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>De Gokker</title>
+</head>
+<body>
+<div class="header">
+<h1>De Gokkers Groep 15</h1>
+
+<video width="320" height="240" controls>
+    <source src="" type="video/mp4">
+    <source src="" type="video/ogg">
+</video>
+</div>
+
+<h2>Welcome, <?php ?></h2>
+
+<h3>Informatie</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nam, quam. Commodi cum dignissimos distinctio,
+    eaque eveniet illum incidunt inventore libero, odio odit perferendis quae ratione temporibus ut vel, vitae?</p>
+
+<h4>Download link</h4>
+</body>
+</html>
