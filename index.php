e723eb80b9f3675dd128bf8756d7e4f1d73817e1
Unknown <D226671@edu.rocwb.nl>
2018-03-12 16:10:59 +0100

    reindent lines

--- index.php
+++ index.php
@@ -10,23 +10,24 @@
 </head>
 <body>
 <div class="header">
-<h1>De Gokkers Groep 15</h1>
+    <h1>De Gokkers Groep 15</h1>
 
-<video width="320" height="240" controls>
-    <source src="videos/PlaceHolderVideo.mp4" type="video/mp4">
-</video>
+    <video width="320" height="240" controls>
+        <source src="videos/PlaceHolderVideo.mp4" type="video/mp4">
+    </video>
 </div>
 <div class="welcome">
-<h2>Welcome, <?php ?></h2>
+    <h2>Welcome, <?php ?></h2>
 </div>
 <div class="info">
-<h3>Informatie</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nam, quam. Commodi cum dignissimos distinctio,
-    eaque eveniet illum incidunt inventore libero, odio odit perferendis quae ratione temporibus ut vel, vitae?</p>
+    <h3>Informatie</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nam, quam. Commodi cum dignissimos
+        distinctio,
+        eaque eveniet illum incidunt inventore libero, odio odit perferendis quae ratione temporibus ut vel, vitae?</p>
 </div>
 <div class="download">
 
-<h4>Download link</h4>
+    <h4>Download link</h4>
 
 </div>
 </body>
