821ce444f73b52b4d28164ef0745756b258ad5f6
Unknown <D226671@edu.rocwb.nl>
2018-03-13 16:15:41 +0100

    add images of hamsters
    Size isn't accurate yet

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>De Gokkers</title>
-</head>
-<body>
-<div class="header">
-    <h1>De Gokkers Groep 15</h1>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>De Gokkers</title>
+    </head>
+    <body>
+    <div class="header">
+        <h1>De Gokkers Groep 15</h1>
+        <img src="img/hamster1.PNG" alt="">
+        <img src="img/hamster2.PNG" alt="">
+        <img src="img/hamster3.PNG" alt="">
+        <img src="img/hamster4.PNG" alt="">
 
-    <video width="320" height="240" controls>
-        <source src="videos/PlaceHolderVideo.mp4" type="video/mp4">
-    </video>
-</div>
-<div class="welcome">
-    <h2>Welcome, <?php ?></h2>
-</div>
-<div class="info">
-    <h3>Informatie</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nam, quam. Commodi cum dignissimos
-        distinctio,
-        eaque eveniet illum incidunt inventore libero, odio odit perferendis quae ratione temporibus ut vel, vitae?</p>
-</div>
-<div class="download">
+        <video width="320" height="240" controls>
+            <source src="videos/PlaceHolderVideo.mp4" type="video/mp4">
+        </video>
+    </div>
+    <div class="welcome">
+        <h2>Welcome, <?php ?></h2>
+    </div>
+    <div class="info">
+        <h3>Informatie</h3>
+        <p>Deze website is bedoeld voor een project genaamd "De Gokkers". Deze applicatie simuleert een race</p>
+        <p> tussen 5 hamsters. Er zijn 3 "Guys" : Sietse, Lidy en Fer. Zij kunnen ieder op 1 hamster wedden. </p>
+        <p>De laatste hamster, die ook Sietse heet verdwijnt na de eerste race.</p>
+    </div>
+    <div class="download">
 
-    <a href="">Download link</a>
+        <a href="">Download link</a>
 
-</div>
-</body>
-</html>
+    </div>
+    </body>
+    </html>
