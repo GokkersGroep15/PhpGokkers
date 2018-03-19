55f3a1db587a3e55ebef570ee514940d4a2f081e
Unknown <D226671@edu.rocwb.nl>
2018-03-19 16:38:37 +0100

    Move download link to the middle and Video to the right

--- index.php
+++ index.php
@@ -1,40 +1,43 @@
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <title>De Gokkers</title>
-    </head>
-    <body>
-    <div class="container">
-        <div class="header">
-            <h1>De Gokkers Groep 15</h1>
-        <img src="img/hamster1.PNG" alt="hamster 1">
-        <img src="img/hamster2.PNG" alt="hamster 2">
-        <img src="img/hamster3.PNG" alt="hamster 3">
-        <img src="img/hamster4.PNG" alt="hamster 4">
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <title>De Gokkers</title>
+        </head>
+        <body>
+        <div class="container">
+            <div class="header">
+                <h1>De Gokkers Groep 15</h1>
+            <img src="img/hamster1.PNG" alt="hamster 1">
+            <img src="img/hamster2.PNG" alt="hamster 2">
+            <img src="img/hamster3.PNG" alt="hamster 3">
+            <img src="img/hamster4.PNG" alt="hamster 4">
 
-        <video width="320" height="240" controls>
-            <source src="videos/DeGokkers.mp4" type="video/mp4">
-        </video>
-    </div>
-    </div>
-    <div class="welcome">
-        <h2>Welcome, <?php ?></h2>
-    </div>
-    <div class="info">
-        <h3>Informatie</h3>
-        <p>Deze website is bedoeld voor een project genaamd "De Gokkers". Deze applicatie simuleert een race</p>
-        <p> tussen 5 hamsters. Er zijn 3 "Guys" : Sietse, Lidy en Fer. Zij kunnen ieder op 1 hamster wedden. </p>
-        <p>De laatste hamster, die ook Sietse heet verdwijnt na de eerste race.</p>
-    </div>
-    <div class="download">
 
-        <a href="">Download link</a>
+        </div>
+        </div>
+        <div class="welcome">
+            <h2>Welcome, <?php ?></h2>
+        </div>
+        <div class="info">
+            <h3>Informatie</h3>
+            <p>Deze website is bedoeld voor een project genaamd "De Gokkers". Deze applicatie simuleert een race</p>
+            <p> tussen 5 hamsters. Er zijn 3 "Guys" : Sietse, Lidy en Fer. Zij kunnen ieder op 1 hamster wedden. </p>
+            <p>De laatste hamster, die ook Sietse heet verdwijnt na de eerste race.</p>
+            <div id="video">
+                <video width="320" height="240" controls>
+                    <source src="videos/DeGokkers.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+        <div class="download">
 
-    </div>
-    </body>
-    </html>
+            <a href="">Download link</a>
+
+        </div>
+        </body>
+        </html>
